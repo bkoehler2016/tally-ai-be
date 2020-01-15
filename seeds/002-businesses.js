a28ddc1b1ad10de4074cd3409195b49d6efc2356
Steve Renner <che1111@hotmail.com>
2020-01-15 15:32:04 -0800

    Updated scripts

---
+++ seeds/002-businesses.js
@@ -0,0 +1,21 @@
+
+exports.seed = function(knex) {
+  
+  return knex('businesses').insert([
+        {
+          id: 1, name: 'First Draft Book Bar', 
+          location: {
+            city: 'Phoenix', 
+            state: 'AZ'
+          }
+        },
+        {
+          id: 2, 
+          name: 'La Gattara Cat Lounge and Boutique', 
+          location: {
+            city: 'Tempe',
+             state: 'AZ'
+            }
+        },
+      ]);
+};
