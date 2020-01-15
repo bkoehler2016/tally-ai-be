8b5391d7302e0a7905d9b0d23206fe7e6ef85161
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-01-15 08:15:15 -0800

    added routes in users-router, added server.use for each router
    Co-authored-by: Steverenner1 <che1111@hotmail.com>
    Co-authored-by: gatrrr <davidgatrmacaulay@gmail.com>

--- api/server.js
+++ api/server.js
@@ -3,8 +3,11 @@ const helmet = require("helmet");
 const cors = require("cors");
 
 // ROUTERS
+const authRouter = require("../auth/auth-router");
+const usersRouter = require("../users/users-router");
 
 // CUSTOM MIDDLEWARE
+const authMiddleware = require("../auth/authenticate-middleware");
 
 const server = express();
 
@@ -12,6 +15,7 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-// server.use('/api/auth/', authRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", authMiddleware, usersRouter);
 
 module.exports = server;
--- users/users-router.js
+++ users/users-router.js
@@ -6,9 +6,9 @@ const router = express.Router();
 
 // CHANGE USER CREDENTIALS
 
-router.put("user/:id", (req, res) => {
+router.put("/:id", (req, res) => {
   const changes = req.body;
-  db.update(req.params.id)
+  db.update(req.params.id, changes)
     .then(user => {
       if (user) {
         res.status(200).json(user);
@@ -21,3 +21,22 @@ router.put("user/:id", (req, res) => {
       res.status(500).json({ message: "Could not update user" }, error);
     });
 });
+
+// DELETE USER
+
+router.delete("/:id", (req, res) => {
+  db.destroy(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: "User deleted" });
+      } else {
+        res.status(404).json({ message: "User could not be found" });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "Could not delete user" }, error);
+    });
+});
+
+module.exports = router;
