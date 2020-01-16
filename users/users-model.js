569fb0aa110e6ec40070403707c461526675b945
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-01-16 15:11:28 -0800

    fixed getUsers and insertBusiness in users-model, added formatUserData in users_helpers, fixed users-router to use those
    Co-authored-by: Steverenner1 <che1111@gmail.com>
    Co-authored-by: gatrrr <davidgatrmacaulay@gmail.com

--- users/users-model.js
+++ users/users-model.js
@@ -8,27 +8,37 @@ module.exports = {
   destroy
 };
 
-async function getUsers(id) {
-  const { business_id, ...rest } = db("users as u")
-
-    .join("users_businesses as ub", "ub.user_id", "=", "u.id")
-    .join("businesses as b", "b.id", "=", "ub.business_id")
-    .where("u.id", "=", { id });
+function getUsers(id) {
+  return db("users as u")
+    .join("users_businesses as ub", "ub.user_id", "u.id")
+    .join("businesses as b", "b.id", "ub.business_id")
+    .join("yelp as y", "y.business_id", "b.id")
+    .where({ "u.id": id })
+    .select("u.first_name", "u.last_name", "b.id", "b.name", "b.city", "b.state", "y.yelp_id", "y.url", "y.image_url")
+    .first();
 }
 
 async function findByBusinessID(id) {
   const result = await db.raw(
-    `SELECT * FROM users_businesses WHERE id = ${id} `
+    `SELECT * FROM businesses WHERE id = ${id} `
   );
   return result[0];
 }
 
-function insertBusiness(business) {
-  return db("user_businesses")
-    .insert(business)
-    .then(id => {
-      return findByBusinessID(id[0]);
-    });
+async function insertBusiness(business) {
+  // Separate yelp data from the rest of the business object
+  const { yelp, ...rest } = business;
+
+  // Insert into businesses table
+  const [id] = await db('businesses').insert({ ...rest });
+
+  // Insert into yelp table after adding business_id
+  const yelp_id = await db('yelp').insert({ ...yelp, business_id: id })
+
+  // Insert into users_businesses table
+  await db('users_businesses').insert({ business_id: id, yelp_id })
+
+  return ({ business_id: id, yelp_id });
 }
 
 function update(id, changes) {
