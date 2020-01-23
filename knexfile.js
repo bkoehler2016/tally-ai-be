7e446a28790af5644fe2fdb7f32a90fa7260efeb
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-01-23 15:31:40 -0800

    set development back to sqlite3

--- knexfile.js
+++ knexfile.js
@@ -3,17 +3,11 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    client: "pg",
+    client: "sqlite3",
     useNullAsDefault: true,
-    connection: process.env.DATABASE_URL,//{
-    //   host: process.env.DB_TESTING_HOST,
-    //   user: process.env.DB_TESTING_USER,
-    //   password: process.env.DB_TESTING_PW,
-    //   database: process.env.DATABASE_TESTING,
-    //   options: {
-    //     port: process.env.DB_TESTING_PORT
-    //   }
-    // },
+    connection: {
+      filename: "./database/testDB.db3"
+    },
     migrations: {
       directory: "./database/migrations"
     },
@@ -23,17 +17,9 @@ module.exports = {
   },
 
   staging: {
-    client: "postgresql",
+    client: "pg",
     useNullAsDefault: true,
-    connection: {
-      host: process.env.DB_STAGING_HOST,
-      user: process.env.DB_STAGING_USER,
-      password: process.env.DB_STAGING_PW,
-      database: process.env.DATABASE_STAGING,
-      options: {
-        port: process.env.DB_STAGING_PORT
-      }
-    },
+    connection: process.env.DATABASE_URL + "?ssl=1",
     pool: {
       min: 2,
       max: 10
@@ -48,20 +34,20 @@ module.exports = {
   },
 
   production: {
-    client: "postgresql",
+    client: "pg",
     useNullAsDefault: true,
-    connection: {
-      host: process.env.DB_PRODUCTION_HOST,
-      user: process.env.DB_PRODUCTION_USER,
-      password: process.env.DB_PRODUCTION_PW,
-      database: process.env.DATABASE_PRODUCTION,
-      options: {
-        port: process.env.DB_PRODUCTION_PORT
-      }
-    },
+    connection: process.env.DATABASE_URL + "?ssl=1",//{
+    //   host: process.env.DB_TESTING_HOST,
+    //   user: process.env.DB_TESTING_USER,
+    //   password: process.env.DB_TESTING_PW,
+    //   database: process.env.DATABASE_TESTING,
+    //   options: {
+    //     port: process.env.DB_TESTING_PORT
+    //   }
+    // },
     pool: {
-      min: 2,
-      max: 10
+      min: 0,
+      max: 7
     },
     migrations: {
       directory: "./database/migrations",
