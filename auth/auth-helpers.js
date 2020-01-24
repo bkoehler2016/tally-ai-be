5cbe5fe912140bbb0bf4b4f81224ab87cf2a99df
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-01-24 10:38:20 -0800

    revived auth-helpers, changed Users.find to Use.findBy

--- auth/auth-helpers.js
+++ auth/auth-helpers.js
@@ -1,49 +1,49 @@
-// const jwt = require("jsonwebtoken");
-// const Users = require("./auth-model");
+const jwt = require("jsonwebtoken");
+const Users = require("./auth-model");
 
-// module.exports = {
-//   getJwtToken,
-//   validateUser
-// };
+module.exports = {
+    getJwtToken,
+    validateUser
+};
 
-// function validateUser(user) {
-//   let errors = [];
+function validateUser(user) {
+    let errors = [];
 
-//   // Check for valid password
-//   if (!user.password || user.password.length < 8) {
-//     errors = [
-//       ...errors,
-//       "Please include a password with at least 8 characters"
-//     ];
-//   }
+    // Check for valid password
+    if (!user.password || user.password.length < 8) {
+        errors = [
+            ...errors,
+            "Please include a password with at least 8 characters"
+        ];
+    }
 
-//   // Check for valid email
-//   if (!user.email || !user.email.includes("@") || !user.email.includes(".")) {
-//     errors = [...errors, "Please include a valid email."];
-//   }
+    // Check for valid email
+    if (!user.email || !user.email.includes("@") || !user.email.includes(".")) {
+        errors = [...errors, "Please include a valid email."];
+    }
 
-//   // Check to see if email already exists in DB
-//   if (Users.find({ email: user.email }).length) {
-//     errors = [...errors, "An account with the provided email already exists."];
-//   }
+    // Check to see if email already exists in DB
+    if (Users.findBy({ email: user.email }).length) {
+        errors = [...errors, "An account with the provided email already exists."];
+    }
 
-//   return {
-//     isSuccessful: errors.length === 0,
-//     errors
-//   };
-// }
+    return {
+        isSuccessful: errors.length === 0,
+        errors
+    };
+}
 
-// function getJwtToken(email, password) {
-//   const payload = {
-//     email,
-//     password
-//   };
+function getJwtToken(email, password) {
+    const payload = {
+        email,
+        password
+    };
 
-//   const secret = process.env.JWT_SECRET || "make sure it's a secret please";
+    const secret = process.env.JWT_SECRET || "make sure it's a secret please";
 
-//   const options = {
-//     expiresIn: "7d"
-//   };
+    const options = {
+        expiresIn: "7d"
+    };
 
-//   return jwt.sign(payload, secret, options);
-// }
+    return jwt.sign(payload, secret, options);
+}
