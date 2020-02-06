75c6dde90feb147f8cc33423bd499b011db4a300
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-02-06 17:04:27 -0500

    Added md from template to readme. Wrote out endpoints and tech stack.

--- auth/auth.spec.js
+++ auth/auth.spec.js
@@ -5,68 +5,69 @@ const server = require('../api/server');
 const db = require('../database/dbConfig');
 
 describe('CRUD Tests', () => {
-    beforeAll(async () => {
-        await db("users").truncate();
-    });
+  beforeAll(async () => {
+    await db("users").truncate();
+  });
 
-      it("tests are running with DB_ENV set to 'testing'", () => {
-        expect(process.env.DB_ENV).toBe("testing");
-    });
+  it("tests are running with DB_ENV set to 'testing'", () => {
+    expect(process.env.ENVIRONMENT).toBe("development");
+  });
 
-describe("auth-router tests", () => {
-        describe("POST /api/auth/register", () => {
-          it("should return a 201 created status", () => {
-            return request(server)
-              .post("/api/auth/register")
-              .send({
-                username: "robert",
-                password: "final",
-                isServiceWorker: 1
-              })
-              .then(res => {
-                expect(res.status).toBe(200);
-              });
+  describe("auth-router tests", () => {
+    describe("POST /api/auth/register", () => {
+      it("should return a 201 created status", () => {
+        return request(server)
+          .post("/api/auth/register")
+          .send({
+            email: "robert2@email.com",
+            password: "password",
+            first_name: "first",
+            last_name: "last"
+          })
+          .then(res => {
+            expect(res.status).toBe(200);
           });
-          it("should return a JSON object after creating a user", () => {
-            return request(server)
-              .post("/api/auth/register")
-              .send({
-                username: "Dave",
-                password: "pass",
-                isServiceWorker: 1
-              })
-              .then(res => {
-                expect(res.type).toEqual("application/json");
-              });
+      });
+      it("should return a JSON object after creating a user", () => {
+        return request(server)
+          .post("/api/auth/register")
+          .send({
+            username: "Dave",
+            password: "pass",
+            isServiceWorker: 1
+          })
+          .then(res => {
+            expect(res.type).toEqual("application/json");
           });
-        });
-    })
+      });
+    });
+  })
 
-    describe("POST /api/auth/login", () => {
-        it("should return a 200 OK status", () => {
-          return request(server)
-            .post("/api/auth/login")
-            .send({
-              username: "Dave",
-              password: "pass",
-              isServiceWorker: 1
-            })
-            .then(res => {
-              expect(res.status).toBe(200);
-            });
-        });
-        it("should return a JSON object", () => {
-          return request(server)
-            .post("/api/auth/login")
-            .send({
-              username: "Dave",
-              password: "pass",
-              isServiceWorker: 1
-            })
-            .then(res => {
-              expect(res.type).toMatch(/json/);
-            });
+  describe("POST /api/auth/login", () => {
+    it("should return a 200 OK status", () => {
+      return request(server)
+        .post("/api/auth/login")
+        .send({
+          username: "Dave",
+          password: "pass",
+          isServiceWorker: 1
+        })
+        .then(res => {
+          expect(res.status).toBe(200);
         });
     });
+    it("should return a JSON object", () => {
+      return request(server)
+        .post("/api/auth/login")
+        .send({
+          username: "Dave",
+          password: "pass",
+          isServiceWorker: 1
+        })
+        .then(res => {
+          expect(res.type).toMatch(/json/);
+        });
+    });
+  });
 
 })
