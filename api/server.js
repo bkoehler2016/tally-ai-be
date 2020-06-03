2085160bfa212d207252bd8a825a950fb66955c7
Michael Phelps <michaeljphelps1978@gmail.com>
2020-06-03 14:02:34 -0500

    adds next() to allow for processes to continue after re.headers are processed (forgot to add in previous commit)

--- api/server.js
+++ api/server.js
@@ -28,6 +28,7 @@ server.use((req, res, next) => {
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
         return res.status(200).json({})
     }
+    next()
 })
 
 server.use("/api/auth", authRouter);
