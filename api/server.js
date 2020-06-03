86fd83095cb360239457e026dfac07ea88df2512
Michael Phelps <michaeljphelps1978@gmail.com>
2020-06-03 13:58:30 -0500

    modifies response headers to satisfy CORS requirements for withCredentials

--- api/server.js
+++ api/server.js
@@ -9,11 +9,26 @@ const usersRouter = require("../users/users-router");
 // CUSTOM MIDDLEWARE
 const authMiddleware = require("../auth/authenticate-middleware");
 
+
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.use((req, res, next) => {
+    res.header(
+        "Access-Control-Allow-Origin", process.env.ORIGIN)
+    res.header(
+        'Access-Control-Allow-Headers',
+        "Origin, X-Requested-With, Content-Type, Accept, Credentials, Authorization"
+    )
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+        return res.status(200).json({})
+    }
+})
 
 server.use("/api/auth", authRouter);
 server.use("/api/users", authMiddleware, usersRouter);
