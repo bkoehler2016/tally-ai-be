c0922efc6a5f4f62d1a8985853a9b6aca421fc9f
Michael Phelps <michaeljphelps1978@gmail.com>
2020-06-09 22:51:44 -0500

    Adds changes to knex file to support testing environment

--- knexfile.js
+++ knexfile.js
@@ -19,10 +19,11 @@ module.exports = {
   testing: {
     client: "pg",
     useNullAsDefault: true,
-    connection: process.env.DATABASE_URL
+    connection: `postgres://${process.env.DB_PRODUCTION_USER}:${process.env.DB_PRODUCTION_PW}@${process.env.DB_PRODUCTION_HOST}:${process.env.DB_PRODUCTION_PORT}/${process.env.DATABASE_TESTING}`,
+
     pool: {
-      min: 2,
-      max: 10
+      min: 0,
+      max: 7
     },
     migrations: {
       directory: "./database/migrations",
@@ -37,16 +38,7 @@ module.exports = {
     client: "pg",
     useNullAsDefault: true,
     connection: `postgres://${process.env.DB_PRODUCTION_USER}:${process.env.DB_PRODUCTION_PW}@${process.env.DB_PRODUCTION_HOST}:${process.env.DB_PRODUCTION_PORT}/${process.env.DATABASE_PRODUCTION}`,
-    //process.env.DATABASE_URL + '?ssl=1',
-    // {
-    //   host: process.env.DB_PRODUCTION_HOST,
-    //   user: process.env.DB_PRODUCTION_USER,
-    //   password: process.env.DB_PRODUCTION_PW,
-    //   database: process.env.DATABASE_PRODUCTION,
-    //   options: {
-    //     port: process.env.DB_PRODUCTION_PORT
-    //   }
-    // },
+
     pool: {
       min: 0,
       max: 7
