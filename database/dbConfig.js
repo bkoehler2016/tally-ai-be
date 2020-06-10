b35311fb214d5f13c828a92a499ca18518271888
Michael Phelps <michaeljphelps1978@gmail.com>
2020-06-09 23:02:24 -0500

    Fixes db config on auth-model. May have to dupe in all models

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,8 +1,9 @@
 const dbEnv = process.env.ENVIRONMENT || 'development'
 
-const knex = require("knex")(dbEnv)
+const knex = require("knex")
 
 const config = require("../knexfile").dbEnv;
 
+const db = knex(config.dbEnv)
 
-module.exports = knex(config);
+module.exports = db
