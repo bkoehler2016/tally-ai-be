aef30988f036d87c032344942ea55ab311b50107
Michael Phelps <michaeljphelps1978@gmail.com>
2020-06-10 22:47:04 -0500

    adding allow-method to header

--- api/server.js
+++ api/server.js
@@ -21,7 +21,7 @@ server.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     )
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
+    res.header("Access-Control-Allow-Methods", "HEAD,GET,PUT,POST,DELETE")
     next()
 })
 
